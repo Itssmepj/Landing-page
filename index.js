b5bd0c5c527963c48f1ea459d21c4d60d0005da0
Pranjal <122043570+Itssmepj@users.noreply.github.com>
2024-02-18 11:09:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("jan 25, 2024 00:00:00").getTime();
+var countDownDate = new Date("june 2, 2024 00:00:00").getTime();
 
 var x = setInterval(function() {
      var now = new Date().getTime();
